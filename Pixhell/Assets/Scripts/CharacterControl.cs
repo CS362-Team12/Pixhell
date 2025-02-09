37c44c646c453abcff382b43ee7c2ac78f4d0532
cdttn99 <135686604+cdttn99@users.noreply.github.com>
2025-02-09 15:13:49 -0800

    Update Movement Branch
    Updated the character for modifiable stats and fixed collision issue as well as too many arrows bug

--- Pixhell/Assets/Scripts/CharacterControl.cs
+++ Pixhell/Assets/Scripts/CharacterControl.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.EventSystems;
+using System.Threading;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,13 +12,27 @@ public class PlayerController : MonoBehaviour
     public InputAction SprintAction;
     Rigidbody2D rigidbody2d;
     Vector2 move;
+    float base_speed = 1.0f;
+    float speed_mult;
     public float speed = 3.0f;
+    public void update_movement(float increase)
+    {
+        speed_mult += increase;
+    }
+    float attack_speed = 1.0f;
+    float attack_speed_mult = 1.0f;
+    float attack_time = Time.time;
+    public void as_increase(float increase)
+    {
+        attack_speed_mult -= increase;
+    }
+
 
     public GameObject projectilePrefab;
 
-    public int max_health = 100;
-    int currentHealth;
-    public int health { get { return currentHealth; } }
+    public float max_health = 100;
+    float current_health;
+    public float health { get { return current_health; } }
 
     public InputAction projectileCreate;
 
@@ -25,7 +40,8 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        currentHealth = 25;
+        speed_mult = base_speed;
+        current_health = max_health;
         rigidbody2d = GetComponent<Rigidbody2D>();
         MoveAction.Enable();
         SprintAction.Enable();
@@ -38,12 +54,12 @@ public class PlayerController : MonoBehaviour
         Vector2 move = MoveAction.ReadValue<Vector2>();
         if (SprintAction.IsPressed())
         {
-            Vector2 position = (Vector2)transform.position + move * 8.0f * Time.deltaTime;
+            Vector2 position = (Vector2)transform.position + move * 8.0f * Time.deltaTime * speed_mult;
             transform.position = position;
         }
         else
         {
-            Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
+            Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime * speed_mult;
             transform.position = position;
         }
         if (projectileCreate.IsPressed()) {
@@ -56,15 +72,29 @@ public class PlayerController : MonoBehaviour
         rigidbody2d.MovePosition(position);
     }
 
-    public void change_health(int health)
+    public void change_health(float health)
     {
-        currentHealth = Mathf.Clamp(currentHealth + health, 0, max_health);
-        Debug.Log(currentHealth + "/" + max_health);
+        current_health = Mathf.Clamp(current_health + health, 0, max_health);
+        Debug.Log(current_health + "/" + max_health);
+    }
+
+    public void update_health(float increase) 
+    {
+        max_health += increase;
+        current_health = max_health;
     }
     void Launch()
     {
-        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
-        Projectile projectile = projectileObject.GetComponent<Projectile>();
-        projectile.Launch(moveDirection, 300);
+        if (Time.time - attack_time >= attack_speed*attack_speed_mult)
+        {
+            attack_time = Time.time;
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0f;
+            Vector2 direction = ((Vector2)(mousePosition - transform.position)).normalized;
+            GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up *.25f, Quaternion.identity);
+            Projectile projectile = projectileObject.GetComponent<Projectile>();
+            projectile.Launch(direction, 4.5f);
+        }
+        
     }
 }

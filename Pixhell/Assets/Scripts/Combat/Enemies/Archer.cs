61aff40bf31b8097ce5ec26d69f294be54c1bed0
NamesAreHard6600 <109428297+NamesAreHard6600@users.noreply.github.com>
2025-02-14 14:26:05 -0800

    Two Movement Options
    Base Movement: Moves towards player
    Archer Movement: Mostly placeholder orbit

--- Pixhell/Assets/Scripts/Combat/Enemies/Archer.cs
+++ Pixhell/Assets/Scripts/Combat/Enemies/Archer.cs
@@ -2,5 +2,21 @@ using UnityEngine;
 
 public class Archer : Enemy
 {
- 
+    public float orbitRadius;  // The distance between the two objects
+    public float orbitSpeed;  // Speed in which its oribit position changes
+
+    float angle = 0f;   // Angle in radians, used for calculating the orbit
+
+    public override void Move() {
+        var player = GameObject.FindWithTag("Player");
+        angle += orbitSpeed * Time.deltaTime;
+
+        // Calculate the new position using trig
+        float x = Mathf.Cos(angle) * orbitRadius;
+        float y = Mathf.Sin(angle) * orbitRadius;
+
+        // Update the position of the object (relative to the target's position)
+        Vector3 target = new Vector3(player.transform.position.x + x, player.transform.position.y + y, transform.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
 }
--- Pixhell/Assets/Scripts/Combat/Enemies/Enemy.cs
+++ Pixhell/Assets/Scripts/Combat/Enemies/Enemy.cs
@@ -3,17 +3,25 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {    
     public float health = 100.0f;
+    public float speed = 1.0f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    // Default, move towards player
+    public virtual void Move() {
+        var player = GameObject.FindWithTag("Player");
+        var step = speed*Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
     }
 
     public void TakeDamage(float damage) {

300c9185f742cd44a3675b00882dbdb42e8e4965
NamesAreHard6600 <109428297+NamesAreHard6600@users.noreply.github.com>
2025-02-16 23:15:33 -0800

    Upgrade Bucket System
    Not fully tested. No choice implemented.

--- Pixhell/Assets/Scripts/GameConstants.cs
+++ Pixhell/Assets/Scripts/GameConstants.cs
@@ -1,7 +1,7 @@
 public static class GameConstants
 {
-    public const int COMMON = 1;
+    public const int COMMON = 0;
     public const int UNCOMMON = 1;
-    public const int RARE = 1;
-    public const int LEGENDARY = 1;
+    public const int RARE = 2;
+    public const int LEGENDARY = 3;
 }
--- Pixhell/Assets/Scripts/Upgrades/LevelUp.cs
+++ Pixhell/Assets/Scripts/Upgrades/LevelUp.cs
@@ -6,7 +6,6 @@ public class LevelUp : MonoBehaviour
     public int experience = 0;
     int[] levelCaps = {0, 3, 10, 15, 22, 30, 40, 55, 70, 90, 100};
     int level = 1;
-    public GameObject upgradeManager;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,7 +20,7 @@ public class LevelUp : MonoBehaviour
             experience -= levelCaps[level];
             level++;
 
-            upgradeManager = GameObject.FindWithTag("UpgradeManager");
+            GameObject upgradeManager = GameObject.FindWithTag("UpgradeManager");
             upgradeManager.GetComponent<UpgradeManager>().TriggerLevelUp();
         }
     }

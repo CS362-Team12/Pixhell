6a5d263bdb8b6a10a55396654bcac4de9929c976
NamesAreHard6600 <109428297+NamesAreHard6600@users.noreply.github.com>
2025-02-23 13:39:16 -0800

    Upgrade UI Pretiness
    Made the UI actually nice to look at. Add more info to the UI, like description and rarity. Not getting the same upgrade multiple times will be implemented when there's more upgrades and upgrades that don't work multiple times.

--- Pixhell/Assets/Scripts/Upgrades/UpgradeController.cs
+++ Pixhell/Assets/Scripts/Upgrades/UpgradeController.cs
@@ -12,7 +12,11 @@ public class UpgradeController : MonoBehaviour
     public Upgrade[][] upgrades;
     // luck is currently not implemented
     public float luck = 0f;
-    string[] textNames = {"Upgrade1Text", "Upgrade2Text", "Upgrade3Text"};
+    string[][] textNames = {
+        new[] {"Upgrade1Title", "Upgrade1Description", "Upgrade1Rarity"},
+        new[] {"Upgrade2Title", "Upgrade2Description", "Upgrade2Rarity"},
+        new[] {"Upgrade3Title", "Upgrade3Description", "Upgrade3Rarity"},
+    };
     Upgrade[] chosenUpgrades = new Upgrade[3];
     float[] odds = {.45f, .35f, .15f, .05f};
     
@@ -60,22 +64,28 @@ public class UpgradeController : MonoBehaviour
         TogglePause();
         for (int i = 0; i < 3; i++) {
             var random = Random.Range(0.0f, 1.0f);
+            var rarity = -1;
+
             if (random < odds[COMMON]) {
-                var random2 = Random.Range(0, upgrades[COMMON].Length);
-                chosenUpgrades[i] = upgrades[COMMON][random2];
+                rarity = COMMON;
             } else if (random < odds[COMMON] + odds[UNCOMMON]) {
-                var random2 = Random.Range(0, upgrades[UNCOMMON].Length);
-                chosenUpgrades[i] = upgrades[UNCOMMON][random2];
+                rarity = UNCOMMON;
             } else if (random < odds[COMMON] + odds[UNCOMMON] + odds[RARE]) {
-                var random2 = Random.Range(0, upgrades[RARE].Length);
-                chosenUpgrades[i] = upgrades[RARE][random2];
+                rarity = RARE;
             } else {
-                var random2 = Random.Range(0, upgrades[LEGENDARY].Length);
-                chosenUpgrades[i] = upgrades[LEGENDARY][random2];
+                rarity = LEGENDARY;
             }
 
-            GameObject text = GameObject.Find(textNames[i]);
-            text.GetComponent<TextMeshProUGUI>().text = chosenUpgrades[i].Title;
+            var random2 = Random.Range(0, upgrades[rarity].Length);
+            chosenUpgrades[i] = upgrades[rarity][random2];
+
+            GameObject title_text = GameObject.Find(textNames[i][0]);
+            title_text.GetComponent<TextMeshProUGUI>().text = chosenUpgrades[i].Title;
+            GameObject description_text = GameObject.Find(textNames[i][1]);
+            description_text.GetComponent<TextMeshProUGUI>().text = chosenUpgrades[i].Description;
+            GameObject rarity_text = GameObject.Find(textNames[i][2]);
+            rarity_text.GetComponent<TextMeshProUGUI>().text = RARITY_STRINGS[rarity];
+            rarity_text.GetComponent<TextMeshProUGUI>().color = RARITY_COLORS[rarity];
         }
     }
 

5c7c5d21c6d026351b81e6aeb4c06959fb5e5f15
James Osborn <james-osborn@hotmail.com>
2025-02-26 20:14:18 -0800

    Health bar working, doesnt display on screens that don't have walk-with-weapon-1

--- Pixhell/Assets/Scripts/Character/HUD/HealthBar.cs
+++ Pixhell/Assets/Scripts/Character/HUD/HealthBar.cs
@@ -6,29 +6,96 @@ using UnityEngine.SceneManagement;
 public class HealthBar : MonoBehaviour
 {
 
-    public Sprite healthChunk;
+    public Sprite healthChunkImg;
     public Image healthBar;
     PlayerController character;
+    GameObject[] bars;
+    int barCount = 100;
+    float barWidth;
+    float spacer = 0f;
+    float healthBarOutline = 8f;
+
+    float prevHealth;
 
     void Start() {
         SceneManager.sceneLoaded += OnSceneLoaded;
-        healthBar = GetComponent<Image>();
-        GameObject characterObj = GameObject.Find("walk-with-weapon-1");
-        character = characterObj.GetComponent<PlayerController>();
+        GenerateHealthBar();
     }
 
     void Update() {
-        
+        if (prevHealth != character.health) {
+            Debug.Log("UPDATING HEALTH");
+            UpdateHealthDisplay();
+         }
+        prevHealth = character.health;
+    }
+
+    void UpdateHealthDisplay() {
+        GameObject characterObj = GameObject.Find("walk-with-weapon-1");
+        character = characterObj.GetComponent<PlayerController>();
+        float percent = character.health / character.max_health;
+        float showing = percent * barCount;
+
+        for (int i = barCount - 1; i >= 0; i--) {
+            bars[i].gameObject.SetActive(i < showing);
+        }
+    }
+
+    void GenerateHealthBar() {
+        healthBar = GetComponent<Image>();
+        GameObject characterObj = GameObject.Find("walk-with-weapon-1");
+        character = characterObj.GetComponent<PlayerController>();
+        prevHealth = character.health;
+
+        bars = new GameObject[barCount];
+        barWidth = (healthBar.rectTransform.rect.width - (2 * healthBarOutline) - (spacer * 2)) / barCount;
+        for (int i = 0; i < barCount; i++) {
+            bars[i] = new GameObject("HealthChunk" + i);
+            Image healthChunk = bars[i].AddComponent<Image>();
+            healthChunk.sprite = healthChunkImg;
+            bars[i].transform.SetParent(healthBar.transform, false);
+
+
+            float width = healthBar.rectTransform.rect.width;
+            RectTransform rectBar = bars[i].GetComponent<RectTransform>();
+            rectBar.sizeDelta = new Vector2(barWidth - spacer, healthBar.rectTransform.rect.height - 5); // Match height
+            rectBar.anchorMin = new Vector2(0, 0.5f);
+            rectBar.anchorMax = new Vector2(0, 0.5f);
+            rectBar.pivot = new Vector2(0, 0.5f); // Left-center pivot
+
+            rectBar.anchoredPosition = new Vector2((i * (rectBar.sizeDelta.x + spacer)) + healthBarOutline, 0);
+        }
+
+        // Overlay border on top
+        GameObject duplicatedSprite = new GameObject("DuplicatedBorder");
+        Image duplicatedHealthChunk = duplicatedSprite.AddComponent<Image>();
+        duplicatedHealthChunk.sprite = healthBar.sprite;
+
+        duplicatedSprite.transform.SetParent(transform, false);
+
+        RectTransform rectDuplicated = duplicatedSprite.GetComponent<RectTransform>();
+        rectDuplicated.sizeDelta = new Vector2(healthBar.rectTransform.rect.width, healthBar.rectTransform.rect.height); // Match height
+        rectDuplicated.anchorMin = new Vector2(0, 0.5f);
+        rectDuplicated.anchorMax = new Vector2(0, 0.5f);
+        rectDuplicated.pivot = new Vector2(0, 0.5f); // Left-center pivot
+        rectDuplicated.anchoredPosition = new Vector2(0, 0);
     }
 
     void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
-        Debug.Log(scene.name);
-        if (scene.name == "StartMenu" || scene.name == "SelectRun") {
-            healthBar.enabled = false;
+        foreach (Transform child in healthBar.transform)
+        {
+            Destroy(child.gameObject);
         }
-        else {
-            healthBar.enabled = true;
+        healthBar.gameObject.SetActive(false);
+        GameObject characterObj = GameObject.Find("walk-with-weapon-1");
+        if (characterObj != null) {
+            character = characterObj.GetComponent<PlayerController>();
+            if (scene.name != "StartMenu" && scene.name != "SelectRun") {
+                GenerateHealthBar();
+                healthBar.gameObject.SetActive(true);
+            }
         }
+    
     }
 
 }

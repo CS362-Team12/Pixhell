88c7d61139a03e100c4391f606f008cca33acf38
cdttn99 <135686604+cdttn99@users.noreply.github.com>
2025-02-27 00:39:00 -0800

    Revert "Merge branch 'main' into Movement"
    This reverts commit 8c4d39bebfeed7c5fd2b5113778e6fbe5016f0be, reversing
    changes made to 3e15e4fd08b48ce6047a5edf3a8d1477ab450829.

--- Pixhell/Assets/Scripts/Character/CharacterControl.cs
+++ Pixhell/Assets/Scripts/Character/CharacterControl.cs
@@ -22,7 +22,6 @@ public class PlayerController : MonoBehaviour
     protected Vector2 move;
     protected bool m_FacingRight = true;
     protected float horizontal_move;
-    public bool is_teleporting = false;
 
     [Header("Move Speed")]
     protected float base_speed = 1.0f;

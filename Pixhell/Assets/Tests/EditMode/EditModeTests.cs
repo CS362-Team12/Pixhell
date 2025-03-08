55840a5e95c2cb23a3a2696e7687b8643092d0ba
James Osborn <james-osborn@hotmail.com>
2025-03-08 01:43:06 -0800

    Testing first test with unity actions

---
+++ Pixhell/Assets/Tests/EditMode/EditModeTests.cs
@@ -0,0 +1,10 @@
+using NUnit.Framework;
+
+public class EditModeTests
+{
+    [Test]
+    public void ExampleTest()
+    {
+        Assert.AreEqual(1, 1);
+    }
+}
---
+++ Pixhell/Assets/Tests/PlayMode/PlayModeTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using System.Collections;
+
+public class ExamplePlayModeTest
+{
+    [UnityTest]
+    public IEnumerator ExampleTest()
+    {
+        Assert.AreEqual(1, 1);
+
+    }
+}

a70d27ba7403155f4f82e4176c24121876ff979e
James Osborn <james-osborn@hotmail.com>
2025-03-09 20:30:53 -0700

    Fixed changes on run file system, added gluttony wave control

--- Pixhell/Assets/Scripts/Menu/RunSelection/LoadCharacterSaves.cs
+++ Pixhell/Assets/Scripts/Menu/RunSelection/LoadCharacterSaves.cs
@@ -40,7 +40,7 @@ public class LoadCharacterSaves : MonoBehaviour
             {
                 string firstLine = reader.ReadLine(); // Read the first line of the file
                 string secondLine = reader.ReadLine();
-                string thirdLine = reader.ReadLine();
+                string dataTime = File.GetLastWriteTime(filePath).ToString("yyyy-MM-dd HH:mm:ss");
 
                 // Instantiate a new button from the prefab
                 GameObject newButton = Instantiate(buttonPrefab.gameObject, panelContainer);
@@ -59,7 +59,7 @@ public class LoadCharacterSaves : MonoBehaviour
                 }
 
                 // Set the button's text
-                buttonText.text = "CLICK TO LOAD \n LOBBY\n\n\n\n" + firstLine + "\n" + secondLine + "\n" + thirdLine;
+                buttonText.text = "CLICK TO LOAD \n LOBBY\n\n\n\n" + dataTime + "\n" + firstLine + "\n" + secondLine + "\n";
                 Button button = newButton.GetComponent<Button>();
                 // Add button functionality
                 button.onClick.AddListener(() => EnterRun(filePath));
@@ -144,10 +144,9 @@ public class LoadCharacterSaves : MonoBehaviour
             string filePath = generalPath + "/" + randomString;
             using (StreamWriter writer = new StreamWriter(filePath))
             {
-                DateTime localDate = DateTime.Now;
-                writer.WriteLine("Last Played: " + localDate);
                 writer.WriteLine("Arena: 1");
                 writer.WriteLine("Coins: 0");
+                writer.WriteLine("Items:");
             }
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
